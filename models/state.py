40b68aed3af9bafbe8de99b34788b91a8bea18bf
CarolinaMolinaSosa <Molinasosacarolina17@gmail.com>
2023-08-14 15:25:11 -0700

    update state

--- models/state.py
+++ models/state.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+from os import getenv
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 from models.city import City
-from os import getenv
-from sqlalchemy import Column, Integer, String, DateTime
-from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = "states"
-    name = Column(String(60), nullable=False)
-    cities = relationship("City", cascade="all, delete", backref="states")
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
 
-    @property
-    def cities(self):
-        """Getter method"""
-        from models import storage
-
-        listOfCities = []
-        for city in storage.all(City):
-            if city.state_id == self.id:
-                listOfCities.append(city)
-        return listOfCities
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("City", backref="state", cascade="delete")
+    else:
+        @ property
+        def cities(self):
+            """Return all cities linked to this relationship."""
+            from models import storage
+            my_list = []
+            for i in list(storage.all(City).values()):
+                if self.id == i.state_id:
+                    my_list.append(i)
+            return my_list
