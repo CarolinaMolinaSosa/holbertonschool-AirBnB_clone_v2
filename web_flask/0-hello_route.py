81b1ae92ebe6a1ade0a5df3ac544f6b93e10c51b
CarolinaMolinaSosa <Molinasosacarolina17@gmail.com>
2023-08-14 08:42:06 -0700

    starts a Flask web application

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def say_hello():
-    """Display “Hello HBNB!” on /."""
+    """Display ""Hello HBNB" on /."""
     return "Hello HBNB!"
 
 
