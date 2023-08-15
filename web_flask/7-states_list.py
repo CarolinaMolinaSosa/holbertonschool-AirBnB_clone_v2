60475b7b4385c53b8eed58c35f57a6ce8082f8d9
CarolinaMolinaSosa <Molinasosacarolina17@gmail.com>
2023-08-15 11:21:50 -0700

    Tasks 7 and template

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
 """Starts a Flask web application"""
-from models import storage
 from flask import Flask
 from flask import render_template
+from models import State
+from models import storage
 
 app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """Remove the current SQLAlchemy session."""
+    storage.close()
 
 
 @app.route("/states_list", strict_slashes=False)
@@ -12,14 +19,8 @@ def states_list():
     """Displays an HTML page with a list of
     all State objects in DBStorage
     """
-    states = storage.all("State")
+    states = storage.all(State)
     return render_template("7-states_list.html", states=states)
-
-
-@app.teardown_appcontext
-def teardown(self):
-    """Remove the current SQLAlchemy session."""
-    storage.close()
 
 
 if __name__ == "__main__":
